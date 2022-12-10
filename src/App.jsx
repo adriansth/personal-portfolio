2c6783c0f4152ad27813aabacb4f8fe033cbeb74
adriansth <soto.herrera.adrian@gmail.com>
2022-12-10 01:28:12 -0600

    build: Implemented React Router for pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+// styles
 import './App.css';
+// react router
+import { Route, Routes } from 'react-router-dom';
+// pages 
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
